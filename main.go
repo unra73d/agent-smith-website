0f993a1c36c3935261dfc0776ee6ffae6387453f
unra73d <10100597+unra73d@users.noreply.github.com>
2025-05-22 21:51:00 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
         http.ServeFile(w, r, "index.html")
     })
 
+    http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "favicon.ico")
+    })
+
     fmt.Println("Server started at http://localhost:80")
     http.ListenAndServe(":80", nil)
 }
